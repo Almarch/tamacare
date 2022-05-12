3f8d808ee3368dae7ddf91356f1deacc5acf1c5e
Almarch <Almarch@users.noreply.github.com>
2022-05-12 20:48:30 +0200

    tamatool dir

--- R/proc_multiple.R
+++ R/proc_multiple.R
@@ -5,6 +5,6 @@ dir_collection = "../collection"
 to_display = sample(dir("../collection"),size = 6, replace = FALSE)
 
 init_multi(all_names = gsub(to_display,pattern = ".bin",replacement = ""),
-           dir_tamatool  = "~/Tamago/tamatool/linux",
+           dir_tamatool  = "~/.../tamatool/linux",
            xmax = 3,
            collec = paste0(dir_collection,"/",to_display))
