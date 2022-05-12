956ecc94541edd7b64560989afae1f0add938f95
Almarch <Almarch@users.noreply.github.com>
2022-05-12 19:59:08 +0200

    Add files via upload

---
+++ R/proc_multiple.R
@@ -0,0 +1,13 @@
+
+source("R/fun.R")
+
+args = (commandArgs(TRUE))
+for(i in 1:length(args)) eval(parse(text = args[[i]]))
+
+all_names =  all_names[1:max(length(dir(dir_collection)))]
+
+init_multi(all_names = all_names,
+           dir_tamatool  = dir_tamatool,
+           xmax = 3,
+           collec = paste0(dir_collection,"/",
+                           sample(dir(dir_collection),length(all_names),F)))
