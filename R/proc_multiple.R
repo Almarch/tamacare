1c6e434ed5ef2f08e2270ceb3d4ef3aef0c1197d
Almarch <Almarch@users.noreply.github.com>
2022-05-12 20:01:39 +0200

    up

--- R/proc_multiple.R
+++ R/proc_multiple.R
@@ -7,7 +7,7 @@ for(i in 1:length(args)) eval(parse(text = args[[i]]))
 all_names =  all_names[1:max(length(dir(dir_collection)))]
 
 init_multi(all_names = all_names,
-           dir_tamatool  = dir_tamatool,
+           dir_tamatool  = dir_tam,
            xmax = 3,
            collec = paste0(dir_collection,"/",
                            sample(dir(dir_collection),length(all_names),F)))
