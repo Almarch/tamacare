10004164bcd4a4d52541f37aa3cac9782006c360
Almarch <Almarch@users.noreply.github.com>
2022-05-12 20:02:15 +0200

    do not use dir

--- R/proc_tamacare.R
+++ R/proc_tamacare.R
@@ -5,13 +5,13 @@ args = (commandArgs(TRUE))
 for(i in 1:length(args)) eval(parse(text = args[[i]]))
 
 init(name = name,
-     dir  = dir,
+      dir_tamatool = dir_tam,
      x    = 0,
      y    = 0,
      filename = to_load)
 
 tamacare(dir_images   = dir_images,
-         dir_tamatool = dir,
+         dir_tamatool = dir_tam,
          name         = name,
          speed        = speed,
          x            = 0,
