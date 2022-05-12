a411951810c9f577db65547e24495094549b5b7d
Almarch <Almarch@users.noreply.github.com>
2022-05-12 20:00:06 +0200

    change "dir"

--- R/proc_clock.R
+++ R/proc_clock.R
@@ -4,7 +4,7 @@ source("R/fun.R")
 args = (commandArgs(TRUE))
 for(i in 1:length(args)) eval(parse(text = args[[i]]))
 
-clock(dir = dir,
+clock(dir_tamatool = dir,
       Time = Time,
       speed = speed,
       filename = paste0(name,".bin"))
