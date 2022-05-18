92292a09b097ff4a3fe9ac76fd1da015d06ae585
Almarch <Almarch@users.noreply.github.com>
2022-05-18 22:01:15 +0200

    gen arg

--- R/proc_clock.R
+++ R/proc_clock.R
@@ -7,6 +7,7 @@ for(i in 1:length(args)) eval(parse(text = args[[i]]))
 clock(dir_tamatool = dir_tam,
       Time = Time,
       speed = speed,
+      gen   = gen,
       filename = paste0(name,".bin"))
 
 
