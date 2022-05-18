4d66e9e99b7bdb644c429c03194063f403dd0593
Almarch <Almarch@users.noreply.github.com>
2022-05-18 19:04:44 +0200

    add gen

--- R/proc_multiple.R
+++ R/proc_multiple.R
@@ -2,9 +2,11 @@
 source("R/fun.R")
 
 dir_collection = "../collection"
-to_display = sample(dir("../collection"),size = 6, replace = FALSE)
+to_display = dir("../collection")
+gen = c("gen1","gen2")[c(1,2,1,1,1,1)]
 
 init_multi(all_names = gsub(to_display,pattern = ".bin",replacement = ""),
            dir_tamatool  = "~/.../tamatool/linux",
+           gen  = gen,
            xmax = 3,
            collec = paste0(dir_collection,"/",to_display))
