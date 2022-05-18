66b5a135c4c3770100b4b91b3ccea21ae7da375a
Almarch <Almarch@users.noreply.github.com>
2022-05-18 19:01:19 +0200

    gen argument

--- R/proc_hatch.R
+++ R/proc_hatch.R
@@ -6,8 +6,7 @@ for(i in 1:length(args)) eval(parse(text = args[[i]]))
 
 init(name = name,
      dir_tamatool = dir_tam,
-     x    = 0,
-     y    = 0,
+     gen = gen,
      filename = paste0(name,".bin"))
 
 hatch(name = name,
